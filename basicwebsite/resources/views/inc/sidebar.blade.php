a61d02e9b46d8d2e22b63dc53e6c53fefb50f2d2
capfer27@gmail.com <Capfer12@hotmail.com>
2018-03-29 12:12:35 +0400

    basicwebsite removed

--- basicwebsite/resources/views/inc/sidebar.blade.php
+++
@@ -1,7 +0,0 @@
-@section('sidebar')
-<!-- <div class="card card-block" style="background-color:#f5f5f5"> -->
-  <div class="card card-block bg-light p-4">
-    <h3>Sidebar</h3>
-    This is the sidebar
-  </div>
-@show
