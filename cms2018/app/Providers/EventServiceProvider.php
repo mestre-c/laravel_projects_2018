5b84c96bf76706b593afcd29e72aed469b7c82f8
capfer27@gmail.com <Capfer12@hotmail.com>
2018-06-27 07:26:01 +0400

    First commit - CMS project started

---
+++ cms2018/app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SundaySim\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'SundaySim\Events\Event' => [
+            'SundaySim\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
