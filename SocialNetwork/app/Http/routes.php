27645d5cad21e2d7315a959ecf08f25c7267c5ac
capfer27@gmail.com <Capfer12@hotmail.com>
2018-06-28 17:20:58 +0400

    HomeController and Template Files created

--- SocialNetwork/app/Http/routes.php
+++ SocialNetwork/app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [
+
+	'uses' => '\SocialNetwork\Http\Controllers\HomeController@index',
+	'as' => 'home',
+
+
+]);
---
+++ SocialNetwork/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('templates.default')
+
+@section('content')
+
+	<h3>Welcome to Social Network - Laravel 5.1</h3>
+	<p>The best social network, like... ever</p>
+
+@stop
+
+
