203ec5ecb880a665262edf5712518354da367ad7
capfer27@gmail.com <Capfer12@hotmail.com>
2018-07-16 19:09:53 +0400

    Table users created

--- chatty/database/migrations/2014_10_12_000000_create_users_table.php
+++ chatty/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email'); //->unique();
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('location')->nullable();
+            $table->rememberToken()->nullable();
+            $table->timestamps();   
         });
     }
 
